184f031ef8b5826ef90f72ece28afc2f5d7c0e8e
Dark4Angel <quakextreme@msn.com>
2024-04-10 13:39:43 -0500

    campo imagen

--- src/main/java/com/digitalab/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/digitalab/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,16 +1,26 @@
 package com.digitalab.springboot.backend.apirest.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 //import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -23,9 +33,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.digitalab.springboot.backend.apirest.models.entity.Cliente;
 import com.digitalab.springboot.backend.apirest.models.services.IClienteService;
@@ -173,15 +185,24 @@ public class ClienteRestController {
 			Cliente clienteActual = clienteService.findById(id);
 			
 			
-
-			
 			if(clienteActual== null) {
 				response.put("mensaje", "Error: no se pudo eliminar, el cliente ID: ".concat(id.toString().concat(" ¡no existe en la base de datos!")));
 				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
 
 			}
 			try {
-					
+
+				Cliente cliente = clienteService.findById(id);
+				String nombreFotoAnterior = cliente.getFoto();
+
+				if(nombreFotoAnterior !=null && nombreFotoAnterior.length() >0) {
+					Path rutaFotoAnterior = Paths.get("uploads").resolve(nombreFotoAnterior).toAbsolutePath();
+					File archivoFotoAnterior = rutaFotoAnterior.toFile();
+					if(archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
+						archivoFotoAnterior.delete();
+
+				       }
+				}
 			     clienteService.delete(id);
 			} catch (DataAccessException e) {
 				
@@ -195,8 +216,74 @@ public class ClienteRestController {
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 			
 	}
-		
-}
+		@PostMapping("clientes/upload")
+		public ResponseEntity<?> upload(@RequestParam("archivo") MultipartFile archivo, @RequestParam("id") Long id){
+			Map<String, Object> response = new HashMap<>();
+			
+			Cliente cliente = clienteService.findById(id);	
+			
+			if (!archivo.isEmpty()) {
+				String nombreArchivo = UUID.randomUUID().toString() + "_" +archivo.getOriginalFilename().replace("", "");
+				Path rutaArchivo = Paths.get("uploads").resolve(nombreArchivo).toAbsolutePath();
+
+				try {
+					Files.copy(archivo.getInputStream(), rutaArchivo);
+				}   catch (IOException e) {
+					response.put("mensaje", "Error al subir la imagen del cliente " +nombreArchivo);
+					response.put("error", e.getMessage().concat(":").concat(e.getCause().getMessage()));
+					return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+				}
+				
+				String nombreFotoAnterior = cliente.getFoto();
+
+				if(nombreFotoAnterior !=null && nombreFotoAnterior.length() >0) {
+					Path rutaFotoAnterior = Paths.get("uploads").resolve(nombreFotoAnterior).toAbsolutePath();
+					File archivoFotoAnterior = rutaFotoAnterior.toFile();
+					if(archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
+						archivoFotoAnterior.delete();
+
+					}
+				}
+				
+				cliente.setFoto(nombreArchivo);
+				
+				clienteService.save(cliente);
+
+				response.put("cliente", cliente);
+				response.put("mensaje", "Has subido correctamente la imagen: " +nombreArchivo);
+				
+			}
+			
+					
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+			
+		}
+		@GetMapping("/uploads/img/{nombreFoto:.+}")
+		public ResponseEntity<Resource> verFoto(@PathVariable String nombreFoto){
+			
+			Path rutaArchivo = Paths.get("uploads").resolve(nombreFoto).toAbsolutePath();
+
+			Resource recurso = null;
+			
+			try {
+				recurso = new UrlResource(rutaArchivo.toUri());
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+			
+			if(!recurso.exists() && !recurso.isReadable()) {
+				throw new RuntimeException("Error no se pudo cargar la imagen: "+ nombreFoto);
+
+			}
+			
+			HttpHeaders cabecera = new HttpHeaders();
+			cabecera.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"");
+			
+			return new ResponseEntity<Resource>(recurso, cabecera, HttpStatus.OK);
+		}
+	}
+
+
 
 
 
--- src/main/java/com/digitalab/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/digitalab/springboot/backend/apirest/models/entity/Cliente.java
@@ -8,12 +8,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
+//import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 
@@ -40,14 +41,18 @@ public class Cliente implements Serializable {
 	@Column(nullable=false, unique=false)
 	private String email;
 	
+	@NotNull(message = "no puede estar vacio")
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
 	
-	@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}
+	
+	private String foto;
+	
+	//@PrePersist
+	//public void prePersist() {
+		//createAt = new Date();
+	//}
 
 	public Long getId() {
 		return id;
@@ -88,6 +93,19 @@ public class Cliente implements Serializable {
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
+	
+	
+	
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+
+
 	/**
 	 * 
 	 */
